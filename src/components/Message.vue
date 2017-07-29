cc5aba99daab24107b7695e73987de278994e0e2
lepfsd <lep.fsd@live.com>
2017-07-29 03:09:54 -0400

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-half">
+        <p>
+          <slot></slot>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  .box{ background: #e3e3e3; padding: 10px; border: 1px solid #c5c5c5; margin-bottom: 1em;}
+</style>
